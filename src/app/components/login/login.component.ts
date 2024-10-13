064a48c4cd615b117c370496cd9681cca8e520ba
Hanine Benali <115839915+Imen-Ht07@users.noreply.github.com>
2024-10-13 11:55:14 +0200

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+// src/app/login/login.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  user: User | undefined;
+  submitted = false;
+  loginError = '';
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  // Login form submission
+  onSubmit() {
+    this.submitted = true;
+
+    // Stop if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
+
+  // Getter for easier access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,19 @@
 export class User {
-}
+    constructor(
+      public name: string,
+      public email: string,
+      public photo: string = 'default.jpg',
+      public role: 'user' | 'project-manager' | 'admin' = 'user',
+      public password: string,
+      public passwordConfirm?: string,
+      public passwordChangedAt?: Date,
+      public passwordResetToken?: string,
+      public passwordResetExpires?: Date,
+      public active: boolean = true
+    ) {}
+  
+    // Method to validate if passwords match
+    public isPasswordConfirmed(): boolean {
+      return this.password === this.passwordConfirm;
+    }
+  }
